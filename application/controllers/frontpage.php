6bb93c2011d98dc953ff0c28968f5ccda89f0b7e
Margaret Manalo <mdymanalo@gmail.com>
2014-07-17 09:59:48 +0800

    initial setup

---
+++ application/controllers/frontpage.php
@@ -0,0 +1,16 @@
+<?php if (!defined('BASEPATH')) die();
+class Frontpage extends Main_Controller {
+
+   public function index()
+	{
+      $this->load->view('include/header');
+      $this->load->view('include/navbar');
+      $this->load->view('include/sidebar');
+      $this->load->view('frontpage');
+      $this->load->view('include/footer');
+	}
+   
+}
+
+/* End of file frontpage.php */
+/* Location: ./application/controllers/frontpage.php */
